0f7e754bb4e466ba9843db7fbe4990fa1e66d9e0
Savrak Bohdan <savrakbohdan@gmail.com>
2021-07-10 12:59:49 +0300

    adding compression

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const path = require("path")
+import compression from "compression"
+import express from "express"
+import * as path from "path"
+
 const app = express()
 
-
-app.use(express.static(__dirname + "/dist/ua-weather-angular"))
+app.use(compression())
+app.use(express.static(path.resolve("./dist/ua-weather-angular")))
 app.listen(process.env.PORT || 8080)
 
+
+
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/ua-weather-angular/index.html"))
+  res.sendFile(
+    path.join(
+      path.resolve("./dist/ua-weather-angular/index.html")
+    )
+  )
 })
